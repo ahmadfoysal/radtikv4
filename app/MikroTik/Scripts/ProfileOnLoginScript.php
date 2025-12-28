a994f4c91824ba3b06938bcbd9874aa7e6d622c8
Ahmad Foysal <ahmadfoysal@gmail.com>
2025-12-28 17:09:44 +0600

    On loign script changed

--- app/MikroTik/Scripts/ProfileOnLoginScript.php
+++ app/MikroTik/Scripts/ProfileOnLoginScript.php
@@ -18,17 +18,12 @@ class ProfileOnLoginScript
 :local u "$user";
 :local m $"mac-address";
 
-# If run from /system script, variables might be empty. Fetch from active list.
+# Recovery: Get data from active list if variables are empty
 :if ([:len $u] = 0) do={
     :set u [/ip hotspot active get [find where address="$address"] user];
 }
-
-# Fetch MAC from active table if missing
 :if ([:len $m] = 0 || $m = "00:00:00:00:00:00") do={
-    :local activeId [/ip hotspot active find where user="$u"];
-    :if ([:len $activeId] > 0) do={
-        :set m [/ip hotspot active get $activeId mac-address];
-    }
+    :set m [/ip hotspot active get [find where user="$u"] mac-address];
 }
 
 /ip hotspot user {
@@ -36,12 +31,19 @@ class ProfileOnLoginScript
     :if ([:len $uid] > 0) do={
         :local comment [get $uid comment];
         
-        # 1. Activation Timestamp
-        :if ([:find "$comment" "ACT="] = nil) do={
+        # 1. Activation Logic (Check if ACT= exists)
+        :local isAct [:find "$comment" "ACT="];
+        
+        # If isAct is empty/nil, it means ACT= not found
+        :if ([:len $isAct] = 0) do={
             :local date [/system clock get date];
             :local time [/system clock get time];
-            set $uid comment=("ACT=$date $time | $comment");
-            :log info ("RADTik: Activation Set for " . $u);
+            :local newTS "ACT=$date $time";
+            
+            set $uid comment=("$newTS | $comment");
+            :log info ("RADTik: Activation Set for $u");
+            # Update local comment variable to include the new ACT tag
+            :set comment ("$newTS | $comment");
         }
 
         # 2. MAC Lock Logic
@@ -50,7 +52,7 @@ class ProfileOnLoginScript
             :if ($smac = "00:00:00:00:00:00" || [:len $smac] = 0) do={
                 :if ([:len $m] > 0) do={
                     set $uid mac-address=$m;
-                    :log info ("RADTik: MAC " . $m . " locked to " . $u);
+                    :log info ("RADTik: MAC $m locked to $u");
                 }
             }
         }
