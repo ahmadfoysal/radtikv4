184dc4e4078c13a9ef86b0b21d5c25edd2817176
Ahmad Foysal <ahmadfoysal@gmail.com>
2025-12-28 12:31:58 +0600

    On loign script changed

--- app/MikroTik/Scripts/PullProfilesScript.php
+++ app/MikroTik/Scripts/PullProfilesScript.php
@@ -11,38 +11,28 @@ class PullProfilesScript
         return 'RADTik-pull-profiles';
     }
 
-    /**
-     * Build RouterOS script that pulls hotspot user profiles
-     * and injects the robust On-Login logic directly.
-     */
     public static function build(Router $router, string $baseUrl): string
     {
         $token = $router->app_key;
 
-        // The full On-Login script as a string for MikroTik
-        // Escaping MikroTik variables (\$var) for PHP HEREDOC
+        // On-Login script with escaped quotes for RouterOS
+        // We use \32 for double quotes to avoid terminal break
         $onLoginScript = '{
             :local u "\$user";
             :local m \$"mac-address";
-
             :if ([:len \$u] = 0) do={:set u [/ip hotspot active get [find where address="\$address"] user]};
-
             /ip hotspot user {
                 :local uid [find where name="\$u"];
                 :if ([:len \$uid] > 0) do={
                     :local comment [get \$uid comment];
-                    
                     :local isAct [:find "\$comment" "ACT="];
-
                     :if ([:len \$isAct] = 0) do={
                         :local date [/system clock get date];
                         :local time [/system clock get time];
                         :local newTS "ACT=\$date \$time";
-                        
                         set \$uid comment=("\$newTS | \$comment");
                         :log info ("RADTik: Activation Set for " . \$u);
                     }
-                    
                     :if ([:find "\$comment" "LOCK=1"] != nil) do={
                         :local smac [get \$uid mac-address];
                         :if (\$smac = "00:00:00:00:00:00" || [:len \$smac] = 0) do={
@@ -54,9 +44,7 @@ class PullProfilesScript
         }';
 
         return <<<SCRIPT
-# RADTik - Pull & Sync Hotspot User Profiles
-# Method: Flat File Parsing with Inline On-Login Logic
-
+# RADTik - Pull & Sync Profiles
 :local baseUrl "{$baseUrl}"
 :local token   "{$token}"
 :local onLoginCmd "{$onLoginScript}"
@@ -64,35 +52,22 @@ class PullProfilesScript
 :local url (\$baseUrl . "?token=" . \$token . "&format=flat")
 :local dst "radtik_profiles.txt"
 
-:log info ("RADTik: Fetching profiles from " . \$baseUrl)
-
 /file remove [find name=\$dst]
-
 /tool fetch url=\$url mode=https http-method=get dst-path=\$dst check-certificate=no keep-result=yes
 :delay 2s;
 
-:if ([:len [/file find name=\$dst]] = 0) do={
-    :log error "RADTik: Fetch failed"
-    :error "fetch failed"
-}
-
 :local content [/file get \$dst contents]
 :local contentLen [:len \$content]
-
-:local lineEnd 0
-:local line ""
 :local lastEnd 0
 :local processed 0
 
 :do {
-    :set lineEnd [:find \$content "\\n" \$lastEnd]
+    :local lineEnd [:find \$content "\\n" \$lastEnd]
     :if ([:typeof \$lineEnd] = "nil") do={ :set lineEnd \$contentLen }
-    :set line [:pick \$content \$lastEnd \$lineEnd]
+    :local line [:pick \$content \$lastEnd \$lineEnd]
     :set lastEnd (\$lineEnd + 1)
 
-    :if ([:pick \$line ([:len \$line]-1)] = "\\r") do={
-        :set line [:pick \$line 0 ([:len \$line]-1)]
-    }
+    :if ([:pick \$line ([:len \$line]-1)] = "\\r") do={ :set line [:pick \$line 0 ([:len \$line]-1)] }
 
     :if ([:len \$line] > 0) do={
         :local s1 [:find \$line ";"]
@@ -103,11 +78,7 @@ class PullProfilesScript
                 :local pShared [:tonum [:pick \$line (\$s1+1) \$s2]]
                 :local s3 [:find \$line ";" (\$s2 + 1)]
                 :local pRate ""
-                :if ([:typeof \$s3] != "nil") do={
-                    :set pRate [:pick \$line (\$s2+1) \$s3]
-                } else={
-                    :set pRate [:pick \$line (\$s2+1) [:len \$line]]
-                }
+                :if ([:typeof \$s3] != "nil") do={ :set pRate [:pick \$line (\$s2+1) \$s3] } else={ :set pRate [:pick \$line (\$s2+1) [:len \$line]] }
 
                 :if (\$pName != "" && \$pName != "default") do={
                     :local existingId [/ip hotspot user profile find name=\$pName]
@@ -122,8 +93,7 @@ class PullProfilesScript
         }
     }
 } while (\$lastEnd < \$contentLen)
-
-:log info ("RADTik: DONE. Total processed: " . \$processed)
+:log info ("RADTik: DONE. Total: " . \$processed)
 /file remove \$dst
 SCRIPT;
     }
